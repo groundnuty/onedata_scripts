9153470bee814f6bf888a71378a9a543301d4104
root <root@xl-openstack-cli.novalocal>
2017-12-28 07:13:39 +0000

    create certs for domain

--- scripts/oneprovider_init.sh
+++ scripts/oneprovider_init.sh
@@ -1,8 +1,38 @@
 #!/bin/bash -x
 
-#Update and Install docker 
 apt-get update
-echo y | apt-get install docker.io
+
+oneprovider_domain=`sed -n -e '/^ONEPROVIDER_DOMAIN/p' /tmp/user-inject.data | awk -F"=" '{print $2}'`
+
+if [ "$oneprovider_domain" != "NO_DOMAIN" ]
+then
+    ping -c 1 -q $oneprovider_domain
+    if [ $? -eq 0 ]
+    then
+        #install certbot & create certs
+        apt-get -y install software-properties-common
+
+        echo "\n" | add-apt-repository ppa:certbot/certbot
+        apt-get update
+        apt-get -y install certbot
+
+        echo 'A' | certbot certonly --standalone -d $oneprovider_domain --register-unsafely-without-email
+
+        mkdir -p /opt/oneprovider/certs
+        cd /opt/oneprovider/certs
+        ln -s /etc/letsencrypt/live/$oneprovider_domain/chain.pem cacert.pem
+        ln -s /etc/letsencrypt/live/$oneprovider_domain/fullchain.pem cert.pem
+        ln -s /etc/letsencrypt/live/$oneprovider_domain/privkey.pem key.pem
+
+        cd /home/ubuntu/onedata/scenarios/3_0_oneprovider_onezone_multihost
+        sed -i 's/#- "${OP_PRIV_KEY_PATH}/- "\/opt\/oneprovider\/certs\/key.pem/' docker-compose-oneprovider.yml
+        sed -i 's/#- "${OP_CERT_PATH}/- "\/opt\/oneprovider\/certs\/cert.pem/' docker-compose-oneprovider.yml
+        sed -i 's/#- "${OP_CACERT_PATH}/- "\/opt\/oneprovider\/certs\/cacert.pem/g' docker-compose-oneprovider.yml
+    fi
+fi
+
+#Install docker
+apt-get -y install docker.io
 
 #Install docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
