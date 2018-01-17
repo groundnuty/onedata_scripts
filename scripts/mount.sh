22897fbc7984c2b52d9a9b311aadc38613e3caea
flypPig <18255531271@163.com>
2018-01-17 08:27:53 +0000

    add mount.sh file to mount sfs

---
+++ scripts/mount.sh
@@ -0,0 +1,33 @@
+#! /bin/bash -x
+
+SFS_SPACE_NAME="SPACE_NAME_1,SPACE_NAME_2"
+SHARE_PATH="SHARE_PATH_1,SHARE_PATH_2"
+
+apt-get -y install nfs-common
+
+ids=`curl -k -u admin:password https://localhost:9443/api/v3/onepanel/provider/spaces | awk -F '\"' '{for (i = 4; i < NF; i+=2) print $i}'`
+
+SPACE_NAME_ARR=(${SFS_SPACE_NAME//,/ })
+SHARE_PATH_ARR=(${SHARE_PATH//,/ })
+
+for ((i=0;i<${#SPACE_NAME_ARR[@]};i++));do
+    space=${SPACE_NAME_ARR[$i]}
+    share_path=${SHARE_PATH_ARR[$i]}
+
+    for line in $ids
+    do
+        if [ $line != "," ]; then
+            space_name=`curl -u admin:password -k  "https://localhost:9443/api/v3/onepanel/provider/spaces/$line" | awk -F '\"' '{print $8}'`
+
+            if [ "$space_name" == "$space" ]; then
+                echo "Space name is "$space
+                mkdir /mnt/oneprovider_data/$line
+                mount -t nfs $share_path /mnt/oneprovider_data/$line
+                break
+            fi
+        fi
+    done
+done
+
+id=`docker ps -a | grep oneprovider | awk '{print $1}'`
+docker restart $id
