def7cf8ec01c6dd75701f4c6e229ba0ba7276735
flypPig <18255531271@163.com>
2018-06-11 09:29:36 +0000

    change oneclient version

--- scripts/oneclient_init.sh
+++ scripts/oneclient_init.sh
@@ -7,11 +7,12 @@ onezone_ip=`sed -n -e '/^ONEZONE_IP/p' /tmp/user-inject.data | awk -F"=" '{print
 oneprovoder_ip=`sed -n -e '/^ONEPROVIDER_IP/p' /tmp/user-inject.data | awk -F"=" '{print $2}'`
 creds=`sed -n -e '/^ONEDATA_CREDS/p' /tmp/user-inject.data | awk -F"=" '{print $2}'`
 
-if [ "$oneclient_version"x != "17.06.0-rc8"x ]
-then
-    cd /home/ubuntu/onedata/scenarios/3_0_oneprovider_onezone_multihost
-    sed -i "s/image: onedata\/oneclient:17.06.0-rc8/image: onedata\/oneclient:$oneclient_version/" docker-compose-onezone.yml
-fi
+rm -rf /home/ubuntu/onedata
+mkdir -p /home/ubuntu/onedata
+
+git clone https://github.com/onedata/getting-started /home/ubuntu/onedata
+cd /home/ubuntu/onedata/scenarios/3_0_oneprovider_onezone_multihost
+sed -i "s/18.02.0.*/$oneclient_version/" docker-compose-oneclient.yml
 
 #Update and Install docker
 apt-get -y install docker.io
@@ -53,7 +54,7 @@ do
     fi
 done
 
-url="https://$onezone_ip:8443/api/v3/onezone/user/client_tokens"
+url="https://$onezone_ip/api/v3/onezone/user/client_tokens"
 token_info=`curl -u $creds -sS --tlsv1.2 -k -H 'Content-type: application/json' -X POST $url`
 
 token=`echo $token_info | awk -F '"' '{print $(NF-1)}'`
