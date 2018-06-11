c8c0ea49e938bb1a3e75b03180f580d376eff6fd
flypPig <18255531271@163.com>
2018-06-11 12:13:14 +0000

    change mount.sh

--- scripts/mount.sh
+++ scripts/mount.sh
@@ -4,6 +4,7 @@ SFS_SPACE_NAME="SPACE_NAME_1,SPACE_NAME_2"
 SHARE_PATH="SHARE_PATH_1,SHARE_PATH_2"
 
 apt-get -y install nfs-common
+apt-get install -y jq
 
 ids=`curl -k -u admin:password https://localhost:9443/api/v3/onepanel/provider/spaces | awk -F '\"' '{for (i = 4; i < NF; i+=2) print $i}'`
 
@@ -17,12 +18,13 @@ for ((i=0;i<${#SPACE_NAME_ARR[@]};i++));do
     for line in $ids
     do
         if [ $line != "," ]; then
-            space_name=`curl -u admin:password -k  "https://localhost:9443/api/v3/onepanel/provider/spaces/$line" | awk -F '\"' '{print $8}'`
+            space_name=`curl -u admin:password -k  "https://localhost:9443/api/v3/onepanel/provider/spaces/$line" | jq '.name' | awk -F '\"' '{print$2}'``
 
             if [ "$space_name" == "$space" ]; then
                 echo "Space name is "$space
                 mkdir /mnt/oneprovider_data/$line
                 mount -t nfs $share_path /mnt/oneprovider_data/$line
+                sleep 2
                 break
             fi
         fi
